049406aa4f85cd1040ba916a3f89bdf579a95de1
Dennis Kimathi <kimathidennis38@gmail.com>
2025-11-01 01:32:48 +0300

    chore: update under construction pages

--- apps/frontend-web/app/(marketing)/cookies-policy/page.tsx
+++ apps/frontend-web/app/(marketing)/cookies-policy/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Container, Heading, Text } from '@chakra-ui/react'
+import { Box, Container, Text } from '@chakra-ui/react'
 import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
 
 export default function Cookies() {
@@ -8,10 +8,11 @@ export default function Cookies() {
     <Container maxW="container.md" py="20">
       <FadeInOnView>
         <Box textAlign="center">
-          <Heading mb="6" size="2xl">
+          <Text fontSize="5xl" fontWeight="extrabold" my="6">
+            {' '}
             🚧 Under Construction 🚧
-          </Heading>
-          <Text color="gray.600" fontSize="xl">
+          </Text>
+          <Text color="gray.400" fontSize="xl">
             This page is currently being built. Please check back soon!
           </Text>
         </Box>
